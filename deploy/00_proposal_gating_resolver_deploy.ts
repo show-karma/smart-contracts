798aaf74e97cff47e448e7e29edf622622c58ff8
Mahesh Murthy <mahesh@mmurthy.com>
2023-09-06 12:16:06 -0700

    Proposal gating resolver

---
+++ deploy/00_proposal_gating_resolver_deploy.ts
@@ -0,0 +1,18 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const {deployments, getNamedAccounts} = hre;
+    const {deploy} = deployments;
+
+    const {deployer} = await getNamedAccounts();
+
+    await deploy('ProposalGatingResolver', {
+        from: deployer,
+        log: true,
+        autoMine: true,
+        args: ["0x4200000000000000000000000000000000000021"]
+    });
+};
+export default func;
+func.tags = ['ProposalGatingResolver']
